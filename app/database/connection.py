861d9990c4e8eeb6cd00e4882802dcf0d2d02740
vitosotdihaet <vitostikitos@gmail.com>
2024-12-18 17:24:37 +0300

    Better Base class syntax

--- app/database/connection.py
+++ app/database/connection.py
@@ -1,13 +1,14 @@
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import DeclarativeBase, sessionmaker
 
 from app.config import settings
 
 engine = create_engine(f'postgresql://{settings.db_user}:{settings.db_password}@{settings.db_host}:{settings.db_port}/{settings.db_name}', echo=True)
 session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-Base = declarative_base()
+class Base(DeclarativeBase):
+    pass
+
 
 def get_connection():
     connection = session()
