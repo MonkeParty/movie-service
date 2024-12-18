11d61d553267f268aa30ff9d5a263b75ebce9383
vitosotdihaet <vitostikitos@gmail.com>
2024-12-18 18:40:36 +0300

    Add a get request

--- app/main.py
+++ app/main.py
@@ -5,6 +5,16 @@ import sqlalchemy as sql
 from app.database.connection import session, get_connection
 from app.config import settings
 
+from app.database.models import Movie
+
 
 
 app = FastAPI()
+
+
+
+@app.get('/{id}')
+def get_movie_info(id: int, db: sql.orm.Session = Depends(get_connection)):
+    statement = sql.select(Movie).filter_by(id=id)
+    return db.execute(statement).scalars().all()
+
